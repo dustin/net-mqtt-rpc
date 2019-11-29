18c3bb02f5973f7b49ea211e5ab77c50e2da5ec8
Dustin Sallings <dustin@spy.net>
2019-11-29 10:36:41 -0800

    Add an option to mqtt-rpc to add a newline after outputting stuff.
    Sometimes I might be returning raw data and don't want this.
    Sometimes I'm returning text and it's annoying to not have a newline.

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@
 
 module Main where
 
+import           Control.Monad        (when)
 import qualified Data.ByteString.Lazy as BL
 import           Data.Maybe           (fromJust)
 import           Network.MQTT.Client
@@ -12,13 +13,14 @@ import           Network.URI
 import           Options.Applicative  (Parser, execParser, fullDesc, help,
                                        helper, info, long, maybeReader, option,
                                        progDesc, short, showDefault, strOption,
-                                       value, (<**>))
+                                       switch, value, (<**>))
 import           System.IO            (stdout)
 
 data Options = Options {
   optUri     :: URI
   , optTopic :: Topic
   , optMsg   :: BL.ByteString
+  , optNL    :: Bool
   }
 
 options :: Parser Options
@@ -26,11 +28,13 @@ options = Options
   <$> option (maybeReader parseURI) (long "mqtt-uri" <> short 'u' <> showDefault <> value (fromJust $ parseURI "mqtt://localhost/") <> help "mqtt broker URI")
   <*> strOption (long "mqtt-topic" <> short 't' <> showDefault <> value "tmp/rpctest" <> help "MQTT request topic")
   <*> strOption (long "mqtt-msg" <> short 'm' <> showDefault <> value "" <> help "Request Message")
+  <*> switch (long "newline" <> help "add a newline after outputting the message")
 
 run :: Options -> IO ()
 run Options{..} = do
   mc <- connectURI mqttConfig{_protocol=Protocol50} optUri
   BL.hPut stdout =<< call mc optTopic optMsg
+  when optNL $ putStrLn ""
 
 main :: IO ()
 main = run =<< execParser opts
